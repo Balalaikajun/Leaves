f04b5eb89c01726f03180573442a0b2656a25b4f
Andreu Klyuev <andrejkluev774@gmail.com>
2024-12-10 13:20:57 +0700

    Попытка развёртывания

--- LeavesTesting/Program.cs
+++ LeavesTesting/Program.cs
@@ -5,7 +5,7 @@ using Data.Models;
 HttpRequestMessage httpRequest = new HttpRequestMessage
 {
     Method = HttpMethod.Get,
-    RequestUri = new Uri("http://localhost:32774/Authentication")
+    RequestUri = new Uri("http://my-app.example.com:30000/Authentication/")
 };
 httpRequest.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJleHAiOjQ4ODYxMzQ5NzZ9.h-wrsuUUmSTgAjDH8uuhxxU8d8mz4nhkBr2__XNj4E4");
 
